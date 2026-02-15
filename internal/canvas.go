82ab9b3e8ce317612517838aa03d1063726e5a1e
milmille <mikemandm222@gmail.com>
2026-02-15 17:10:47 -0600

    testing out adding a border around the map

--- internal/canvas.go
+++ internal/canvas.go
@@ -27,12 +27,27 @@ func (c *Canvas) PaintPixel(x int, y int, style tcell.Style) {
 }
 
 func (c *Canvas) Draw() {
-	for x := range *c.Cells {
-		for y := range (*c.Cells)[x] {
-			pixel := (*c.Cells)[x][y]
-			rune, _, _, _ := c.screen.GetContent(x, y)
-			if rune == ' ' {
-				c.screen.SetContent(x, y, OctantRunes[pixel.character], nil, pixel.style)
+	borderStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorReset)
+	for x := 0; x < len(*c.Cells); x++ {
+		for y := 0; y < len((*c.Cells)[x]); y++ {
+			if x == 0 && y == 0 {
+				c.screen.SetContent(x, y, 0x250c, nil, borderStyle)
+			} else if x == len(*c.Cells)-1 && y == 0 {
+				c.screen.SetContent(x, y, 0x2510, nil, borderStyle)
+			} else if x == 0 && y == len((*c.Cells)[x])-1 {
+				c.screen.SetContent(x, y, 0x2514, nil, borderStyle)
+			} else if x == len(*c.Cells)-1 && y == len((*c.Cells)[x])-1 {
+				c.screen.SetContent(x, y, 0x2518, nil, borderStyle)
+			} else if x == 0 || x == len(*c.Cells)-1 {
+				c.screen.SetContent(x, y, 0x2502, nil, borderStyle)
+			} else if y == 0 || y == len((*c.Cells)[x])-1 {
+				c.screen.SetContent(x, y, 0x2500, nil, borderStyle)
+			} else {
+				pixel := (*c.Cells)[x][y]
+				rune, _, _, _ := c.screen.GetContent(x, y)
+				if rune == ' ' {
+					c.screen.SetContent(x, y, OctantRunes[pixel.character], nil, pixel.style)
+				}
 			}
 		}
 	}
